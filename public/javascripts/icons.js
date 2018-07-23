3b3ec806fea83f0e8358594a32c11b9940c00c9e
Daviel Frias <yfriasgarcia@gmail.com>
2018-07-23 14:08:39 +0200

    version uploade gitHub

---
+++ public/javascripts/icons.js
@@ -0,0 +1,28 @@
+const icons = [
+   './images/icons/animals.png',
+   './images/icons/medical-2.png',
+   './images/icons/medical-3.png',
+   './images/icons/medical-4.png',
+   './images/icons/medical-5.png',
+   './images/icons/medical-6.png',
+   './images/icons/medical-7.png',
+   './images/icons/medical-8.png',
+   './images/icons/medical-9.png',
+   './images/icons/medical-10.png',
+   './images/icons/medical-11.png',
+   './images/icons/medical-12.png',
+   './images/icons/medical-13.png',
+   './images/icons/medical-14.png',
+   './images/icons/medical.png',
+   './images/icons/people-1.png',
+   './images/icons/people-2.png',
+   './images/icons/people-3.png',
+   './images/icons/people-4.png',
+   './images/icons/people-5.png',
+   './images/icons/people.png',
+   './images/icons/transport.png',
+
+
+];
+
+module.exports = icons
